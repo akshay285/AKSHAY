d143f3462a22b07f14b8bf62bfa48b675ebae152
akshay285 <84015151+akshay285@users.noreply.github.com>
2023-08-11 10:52:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "26992289"))
 API_HASH = os.environ.get("API_HASH", "5d7806f8eaa060d1f68843772b9d67cc")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "1001691644684"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001691644684"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1323102103"))
@@ -28,11 +28,11 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1323102103"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kaushalxraja:pBgB9K@cluster0.dfvemtb.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "1001691644684"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001691644684"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
